76f1d82ddff90d1ae864a96bb5702a728a132c38
Ronny <ronny_loekken@hotmail.com>
2018-09-15 14:27:11 +0200

    Experimenting with design ideas

--- init.js
+++ init.js
@@ -1,3 +1,46 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    $("#sideMenu").width("50em");
+    $("#sideMenu").data("hidden", false);
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    $("#sideMenu").width(0);
+    $("#sideMenu").data("hidden", true);
+}
+
+$("#i_settings").click(function() {
+    let sideMenu = $("#sideMenu");
+    if (sideMenu.data("hidden") === true) {
+        openNav();
+        $("#i_settings").html("close");
+    } else {
+        closeNav();
+        $("#i_settings").html("menu");
+    }
+   //chrome.tabs.create({url: "options.html"});
+});
+
+$("#settingsMenuCloseBtn").click(function() {
+    closeNav();
+});
+
 chrome.storage.sync.get(['hassUrl', 'hassPass'], function(result) {
     if (result !== undefined && result.hassUrl !== undefined && result.hassPass !== undefined) {
         let hassUrl = result.hassUrl;
